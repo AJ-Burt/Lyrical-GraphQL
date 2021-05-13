495541d5502c0b0c5c3d73657ab253c2a71736e0
ajburt <aj@ajburt.com>
2021-05-13 10:34:20 -0600

    Make links clickable

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,13 +1,25 @@
 import React, {Component} from 'react';
+import {graphql} from 'react-apollo';
+import {Link} from 'react-router';
+import fetchSong from '../queries/fetchSong';
 
 class SongDetail extends Component {
     render() {
+        const {song} = this.props.data;
+
+        // Disabled because loading is so fast
+        // if(!song) {
+        //     return <div>Loading...</div>
+        // }
+
         return(
             <div>
-                <h3>Song Detail</h3>
+                <h3>{song.title}</h3>
             </div>
         )
     }
 }
 
-export default SongDetail;
+export default (fetchSong, {
+    options: (props) => {return {variables: {id: props.params.id}}}
+})(SongDetail);
